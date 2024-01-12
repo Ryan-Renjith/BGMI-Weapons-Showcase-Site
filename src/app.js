bf587ad2e07608085e46185c8de9b7df722df2d9
RyanRenjith <ryanrenjith19@gmail.com>
2024-01-12 08:29:51 +0530

    Created template for gun cards and section for each gun category

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ document.addEventListener('scroll', () => {
     if(window.scrollY > 150) {
         navbar.classList.add('transition');
         navbar.classList.add('ease-out');
-        navbar.classList.add('bg-green-700');
+        navbar.classList.add('bg-black');
         navbar.classList.add('duration-700');
     } else {
-        navbar.classList.remove('bg-green-700');
+        navbar.classList.remove('bg-black');
     }
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        'cardW': '300px',
+      }
+    },
   },
   plugins: [],
 }
