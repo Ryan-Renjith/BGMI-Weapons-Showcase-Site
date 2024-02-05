3d368b3a9ac5bc130aa9e89da49df7446faceffc
RyanRenjith <ryanrenjith19@gmail.com>
2024-02-05 13:45:53 +0530

    Dropdown menu created for smaller screens and navbar functionality added

--- src/app.js
+++ src/app.js
@@ -10,3 +10,39 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('bg-black');
     }
 });
+
+function toggleDropdown() {
+    var dropdown = document.getElementById("myDropdown");
+    dropdown.classList.toggle("hidden");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('#menu')) {
+        var dropdowns = document.getElementsByClassName("dropdown");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (!openDropdown.classList.contains('hidden')) {
+                openDropdown.classList.add('hidden');
+            }
+        }
+    }
+}
+
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+
+    if (section) {
+        section.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+}
+
+function showInfo(sectionId) {
+    var section = document.getElementById(sectionId);
+
+    if(section) {
+        section.classList.toggle('hidden');
+    }
+}
